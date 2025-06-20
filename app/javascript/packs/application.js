44b31f4257445f1eea44ab1db784cc7da171ab0b
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-06-20 21:05:36 +0000

    [Add] homes_controller bootstrap scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import "popper.js"
 import "bootstrap";
 import "../stylesheets/application";
 import "../stylesheets/application";
+import "../stylesheets/mystyle.scss"
 
 Rails.start()
 Turbolinks.start()
