0d4c4c50045766af3731eb1895fb6b46350512f8
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-07-11 21:06:00 +0000

    [Add] posts_controller_new_index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,28 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    post.save
+    redirect_to top_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :price, :day, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
+ 
   root 'homes#top' ,  as: 'top'
   get 'homes/about' , as: 'about'
+  resources :posts
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
