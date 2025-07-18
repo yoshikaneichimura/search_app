ecbd8bf8cd9097829a437544ccfda70ba4b5b8eb
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-07-18 21:07:26 +0000

    [Add] posts_controller_show_edit_destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,9 +15,23 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path
+  end
+  
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path 
   end
 
   private
