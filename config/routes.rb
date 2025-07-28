7f424e9e6151bd86a23cf29ff560588cee0fa464
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-07-28 21:10:15 +0000

    [Add] users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'homes#top' ,  as: 'top'
   get 'homes/about' , as: 'about'
   resources :posts
+  resources :users, only: [:index, :show, :edit] 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
