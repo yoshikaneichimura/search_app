447ce285138ed49dfb11a8db67c30a01e545eba5
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-08-22 06:08:12 +0000

    [Add] users_controller_index_show_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
- 
-  root 'homes#top' ,  as: 'top'
+  root 'homes#top'  , as: 'top'
   get 'homes/about' , as: 'about'
-  resources :posts
-  resources :users, only: [:index, :show, :edit] 
   devise_for :users
+  resources :posts
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2025_06_27_223009) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "name"
+    t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
