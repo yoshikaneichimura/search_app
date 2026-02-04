7979e22acbccfa6e0cf0b59fca510f699856468c
yoshikaneichimura <yoshikanemobile@gmail.com>
2026-02-04 13:08:45 +0000

    [Fix] Post_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable
 
-  has_many :post_images, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
